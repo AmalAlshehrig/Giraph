29f50533dad52d9b3b5a44fd235eb4166138b88e
ghadah2019 <ghada__o@hotmail.com>
2023-12-26 20:37:09 +0300

    new

--- my-project/src/page/Rout.jsx
+++ my-project/src/page/Rout.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import{Routes, Route } from 'react-router-dom';
 import Home from './Home';
+import Signup from './Signup'
+import Login from './Login'
 function Rout() {
   return (
     <div>
         <Routes>
             <Route path='/' element={<Home/>}/>
+            <Route path='/Signup' element={<Signup/>}></Route>  
+      <Route path='/Login' element={<Login/>}></Route>  
         </Routes>
     </div>
   )
