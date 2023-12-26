eae169449629516adfe04e1e2f8fb4cd37d6364b
kira <arooji25@live.com>
2023-12-26 23:35:17 +0300

    add

---
+++ my-project/src/page/Error.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/sOHuH7tdi0VTAYfx/scene.splinecode" />
+  );
+}
